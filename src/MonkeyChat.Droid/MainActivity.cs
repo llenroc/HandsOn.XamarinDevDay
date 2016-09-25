38bb9927c9176f12eb519fcc90909f76d69ab68f
Ariel De Los Santos <landazuri69@gmail.com>
2016-09-25 12:12:48 -0400

    commit the solutions of the Xamarin dev day tasks

---
+++ src/MonkeyChat.Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using ImageCircle.Forms.Plugin.Droid;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+
+namespace MonkeyChat.Droid
+{
+    [Activity(Label = "Monkey Chat", Icon = "@mipmap/ic_launcher", Theme= "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            ToolbarResource = Resource.Layout.toolbar;
+            TabLayoutResource = Resource.Layout.tabs;
+
+            base.OnCreate(bundle);
+            Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
+            ImageCircleRenderer.Init();
+
+            DependencyService.Register<ITwilioMessenger, TwilioMessenger>();
+
+            LoadApplication(new App());
+        }
+
+    }
+}
+
